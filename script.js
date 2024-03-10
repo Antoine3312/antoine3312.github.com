bc79f2d219ae4766d98649ab93014d1c6341445e
Antoine <antoine.mazeau31@gmail.com>
2024-03-10 11:23:58 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const animationDuration = 300
 const burgerMenu = document.getElementById('burger_menu');
 showMenu = () => {
-    // burgerMenu.style.animation = " slideIn ." + animationDuration + "s 0s cubic-bezier(0.9, -0.01, 0.17, 1)";
-    // burgerMenu.style.display = "flex";
+    burgerMenu.style.animation = " slideIn ." + animationDuration + "s 0s cubic-bezier(0.9, -0.01, 0.17, 1)";
+    burgerMenu.style.display = "flex";
 }
 closeMenu = () => {
     burgerMenu.style.animation = " slideOut ." + animationDuration + "s 0s cubic-bezier(0.9, 0, 0.22, 1)";
